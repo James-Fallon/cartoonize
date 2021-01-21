411696ec661d47ceaeef3e73ba1ca5ff6935270b
JamesFallon <jamesfal27@hotmail.com>
2021-01-21 20:46:59 +1100

    feat: added Dockerfile to deploy the flask api as a docker container

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+# Using the base python image
+FROM python:3.7.9
+
+WORKDIR /code
+
+# Install requirements
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+
+# Copy source code over
+COPY cartoonize/ /code/cartoonize
+COPY setup.py /code/
+
+# Install our cartoonize package
+RUN python setup.py install
+
+# Set directory for image uploads
+ENV CARTOONIZE_IMAGE_DIR=/images
+
+# Run the flask app
+ENTRYPOINT ["python"]
+CMD ["cartoonize/api.py"]
--- cartoonize/api.py
+++ cartoonize/api.py
@@ -5,7 +5,7 @@ from flask_restful import Api
 
 from cartoonize.image import ImageList, Image, Cartoonize
 
-IMAGE_DIR = '/Users/jamesfallon/cartoonize/images/'
+IMAGE_DIR = os.environ.get('CARTOONIZE_IMAGE_DIR', '/Users/jamesfallon/cartoonize/images/')
 if os.path.exists(IMAGE_DIR) is False:
     os.mkdir(IMAGE_DIR)
 
@@ -17,4 +17,4 @@ api.add_resource(Image, '/images/<image_id>', resource_class_kwargs={'image_fold
 api.add_resource(Cartoonize, '/images/<image_id>/cartoonize')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
