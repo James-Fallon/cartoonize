56bc95f784c17cefd34c8ab5670d89273d7be9b2
JamesFallon <jamesfal27@hotmail.com>
2021-01-22 12:04:04 +1100

    docs: rename images_dict to images_library to try and cover up how crude it is lol

--- cartoonize/data_models/storage/image_local_storage.py
+++ cartoonize/data_models/storage/image_local_storage.py
@@ -5,20 +5,20 @@ from uuid import uuid4
 class ImageLocalStorage(object):
     def __init__(self, folder_path):
         self.folder_path = folder_path
-        self.images_dict = {}
+        self.images_library = {}
 
     def find_one(self, image_id):
-        result = self.images_dict.get(image_id)
+        result = self.images_library.get(image_id)
         if result is None:
             return None
         path = os.path.join(self.folder_path, result)
         return path
 
     def find_all(self):
-        return self.images_dict
+        return self.images_library
 
     def insert_one(self, image_file, file_name):
         image_id = str(uuid4())
-        self.images_dict[image_id] = file_name
+        self.images_library[image_id] = file_name
         image_file.save(os.path.join(self.folder_path, file_name))
         return image_id
